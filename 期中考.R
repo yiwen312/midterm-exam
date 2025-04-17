8a5d1046d7fe1f4332f2d7fe7cd863e489f9bcba
Evon <09220491@me.mcu.edu.tw>
2025-04-17 14:56:55 +0800

    Questions 5、6、7

--- 期中考.R
+++ 期中考.R
@@ -24,3 +24,16 @@ g+geom_point()+ggtitle("點陣圖") + xlab("手機重量") + ylab("電池容量"
 library(car)
 leveneTest(PriceUSA~Company, data=df) #p值=5.218e-14 < 0.05，拒絕H0，表示不同廠牌中美國售價之變異數不相等
 
+#第五題
+aov1 <- aov(Weight~Company, data=df) 
+summary(aov1) #p值=3.55e-05 < 0.05，結果具有統計顯著性，拒絕H0，表示不同廠牌中手機重量之變異數不相等
+
+#第六題
+aov2 <- aov(Weight~Company+Battery, data=df) 
+summary(aov2) 
+#Company的p值<2e-16，表示廠牌對手機重量具顯著差異
+#Battery的p值<2e-16，表示電池容量對手機重量具顯著差異
+
+#第七題
+nrow(df[df$Year==2024 & df$Company=="Apple",])/nrow(df[df$Year==2024,]) #0.12
+
